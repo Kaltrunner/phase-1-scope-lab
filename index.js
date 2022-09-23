80d35e17098d556b46e3208f2b6bd3ed79cb37b0
Erik Russell <erikrussells@gmail.com>
2022-09-23 12:00:23 -0400

    commit tree fc7df524a300e90741355d08375a42b013a027ff

--- index.js
+++ index.js
@@ -1,1 +1,67 @@
 // Write your solution in this file!
+// var customerName = "bob";
+
+// function upperCaseCustomerName() {
+//     return customerName = customerName.toUpperCase();
+// }
+
+// function setBestCustomer() {
+//     bestCustomer = "not bob";
+// }
+
+// function overwriteBestCustomer() {
+//     bestCustomer = "maybe bob";
+// }
+
+// const leastFavoriteCustomer = "Kaltrunner"
+// function changeLeastFavoriteCustomer() {
+//     leastFavoriteCustomer = "bob";
+// }
+
+
+
+
+
+
+
+// var customerName = 'bob'
+// const leastFavoriteCustomer = 'Erik'
+
+// function upperCaseCustomerName() {
+//     customerName = customerName.toUpperCase()
+// }
+
+// function setBestCustomer() {
+//     bestCustomer = 'not bob'
+// }
+
+// function overwriteBestCustomer() {
+//     bestCustomer = 'maybe bob'
+// }
+
+// function changeLeastFavoriteCustomer() {
+//     leastFavoriteCustomer = 'Kaltrunner'
+// }
+
+
+
+
+
+var customerName = "bob";
+
+function upperCaseCustomerName() { 
+customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer()  {
+    return bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer() {
+    return bestCustomer = "maybe bob";
+}
+
+const leastFavoriteCustomer = 'Erik'
+function changeLeastFavoriteCustomer() {
+    return leastFavoriteCustomer = "bob"
+}
